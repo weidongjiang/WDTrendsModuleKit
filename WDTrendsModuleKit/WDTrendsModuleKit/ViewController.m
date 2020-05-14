b834315c2ad99e815e051f40c10cc4cd9cea9537
yixiajwd <yixiajwd@yixiajwddeMacBook-Pro.local>
2020-05-14 15:47:04 +0800

    布局优化

--- WDTrendsModuleKit/WDTrendsModuleKit/ViewController.m
+++ WDTrendsModuleKit/WDTrendsModuleKit/ViewController.m
@@ -51,7 +51,10 @@ static NSString *alpha_key = @"alpha";
 
 @interface ViewController ()
 
-@property (nonatomic, strong) NSMutableDictionary            *itemDictionary; ///< <#value#>
+@property (nonatomic, strong) NSMutableDictionary            *itemInfoDictionary; ///< <#value#>
+@property (nonatomic, strong) NSMutableDictionary            *itemObjDictionary; ///< <#value#>
+
+@property (nonatomic, strong) NSMutableArray            *itemTempArray; ///< <#value#>
 
 
 @end
@@ -62,13 +65,6 @@ static NSString *alpha_key = @"alpha";
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 
-
-
-    UIButton *itemButton = [[UIButton alloc] init];
-    [itemButton setTitle:@"jj" forState:UIControlStateNormal];
-    [itemButton setBackgroundColor:[UIColor yellowColor]];
-
-
     [self creatUI];
 
 }
@@ -78,37 +74,66 @@ static NSString *alpha_key = @"alpha";
 
     NSArray *uiArray = [self getTrendsModulePlistInfo];
 
-    for (NSDictionary *obj in uiArray) {
+    for (NSDictionary *info in uiArray) {
 
-        NSString *itemType = [obj objectForKey:itemType_key];
-        NSString *itemID = [obj objectForKey:itemID_key];
-        NSString *itemSuperview = [obj objectForKey:itemSuperview_key];
+        NSString *itemType = [info objectForKey:itemType_key];
 
 
+        // 创建
         UIView *item = [self _alloc_init_itemType:itemType];
-        [self.itemDictionary setValue:item forKey:itemID];
-
-        if ([itemSuperview isEqualToString:@"default"]) {
-            [self.view addSubview:item];
-        }else {
-            UIView *_itemSuperview = [self.itemDictionary objectForKey:itemSuperview];
-            if (!_itemSuperview) {
-                return;
-            }
-            [_itemSuperview addSubview:item];
-        }
-
-        NSDictionary *style = [obj objectForKey:style_key];
+        // 设置样式
+        NSDictionary *style = [info objectForKey:style_key];
         item = [self _setItemStyle:style item:item];
 
-        NSDictionary *layout = [obj objectForKey:layout_key];
-        item = [self _steItemLayout:layout item:item];
+        // 内存ID
+        NSString *iteminfoID = [NSString stringWithFormat:@"%p",item];
+        // 标记ID
+        NSString *itemID = [info objectForKey:itemID_key];
+
+        // 跟进内存ID存储 配置信息
+        [self.itemInfoDictionary setValue:info forKey:iteminfoID];
+        // 跟进标记ID 存储对象
+        [self.itemObjDictionary setValue:item forKey:itemID];
+        // 临时存储对象
+        [self.itemTempArray addObject:item];
+
+    }
+
+
+    for (UIView *item in self.itemTempArray) {
+
+        // 获取配置信息
+        NSString *iteminfoID = [NSString stringWithFormat:@"%p",item];
+        NSDictionary *info = [self.itemInfoDictionary objectForKey:iteminfoID];
+
+        // 根据标记ID 获取对象的 superView
+        NSString *item_superview = [info objectForKey:itemSuperview_key];
+        UIView *_superview = [self.itemObjDictionary objectForKey:item_superview];
+        
+        if ([item_superview isEqualToString:@"default"]) {
+            [self.view addSubview:item];
+        }else {
+            if (!_superview) {
+                continue;
+            }
+            [_superview addSubview:item];
+        }
+
+        NSDictionary *layout = [info objectForKey:layout_key];
+        [self _steItemLayout:layout item:item];
 
     }
 
 }
 
 - (UIView *)_steItemLayout:(NSDictionary *)layout item:(UIView *)item {
+
+    if (!item) {
+        return nil;
+    }
+    if (!item.superview) {
+        return nil;
+    }
 
 
     CGFloat width = [[layout objectForKey:width_key] floatValue];
@@ -143,12 +168,55 @@ static NSString *alpha_key = @"alpha";
     CGFloat benchmark_centerY_offset = [[layout objectForKey:benchmark_centerY_offset_key] floatValue];
 
 
-    [item mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.width.mas_equalTo(width);
-        make.height.mas_equalTo(height);
-        make.left.equalTo(item.superview).offset(benchmark_left_offset);
-        make.top.equalTo(item.superview).offset(benchmark_top_offset);
-    }];
+    if (width > 0 && height > 0 && benchmark_top && benchmark_left) {
+        [item mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.width.mas_equalTo(width);
+            make.height.mas_equalTo(height);
+            make.left.equalTo(item.superview).offset(benchmark_left_offset);
+            make.top.equalTo(item.superview).offset(benchmark_top_offset);
+        }];
+    }else if (width > 0 && height > 0 && benchmark_bottom && benchmark_left) {
+        [item mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.width.mas_equalTo(width);
+            make.height.mas_equalTo(height);
+            make.left.equalTo(item.superview).offset(benchmark_left_offset);
+            make.bottom.equalTo(item.superview).offset(benchmark_bottom_offset);
+        }];
+    }else if (width > 0 && height > 0 && benchmark_bottom && benchmark_right) {
+        [item mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.width.mas_equalTo(width);
+            make.height.mas_equalTo(height);
+            make.right.equalTo(item.superview).offset(benchmark_right_offset);
+            make.bottom.equalTo(item.superview).offset(benchmark_bottom_offset);
+        }];
+    }else if (width > 0 && height > 0 && benchmark_top && benchmark_right) {
+        [item mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.width.mas_equalTo(width);
+            make.height.mas_equalTo(height);
+            make.right.equalTo(item.superview).offset(benchmark_right_offset);
+            make.top.equalTo(item.superview).offset(benchmark_top_offset);
+        }];
+    }else if (width > 0 && height > 0 && benchmark_center) {
+        [item mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.width.mas_equalTo(width);
+            make.height.mas_equalTo(height);
+            make.center.equalTo(item.superview).offset(benchmark_center_offset);
+        }];
+    }else if (width > 0 && height > 0 && benchmark_centerX && benchmark_centerY) {
+        [item mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.width.mas_equalTo(width);
+            make.height.mas_equalTo(height);
+            make.centerX.equalTo(item.superview).offset(benchmark_centerX_offset);
+            make.centerY.equalTo(item.superview).offset(benchmark_centerY_offset);
+        }];
+    }else if (benchmark_top && benchmark_left && benchmark_bottom && benchmark_right) {
+        [item mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(item.superview).offset(benchmark_top_offset);
+            make.left.equalTo(item.superview).offset(benchmark_left_offset);
+            make.bottom.equalTo(item.superview).offset(benchmark_bottom_offset);
+            make.right.equalTo(item.superview).offset(benchmark_right_offset);
+        }];
+    }
 
     return item;
 }
@@ -159,7 +227,6 @@ static NSString *alpha_key = @"alpha";
     CGFloat alpha = [[style objectForKey:alpha_key] floatValue];
 
     [item setBackgroundColor:[UIColor wdt_colorWithHex:backgroundColor alpha:alpha]];
-
 
     return item;
 }
@@ -196,11 +263,25 @@ static NSString *alpha_key = @"alpha";
 
 }
 
-- (NSMutableDictionary *)itemDictionary {
-    if (!_itemDictionary) {
-        _itemDictionary = [[NSMutableDictionary alloc] init];
+- (NSMutableDictionary *)itemInfoDictionary {
+    if (!_itemInfoDictionary) {
+        _itemInfoDictionary = [[NSMutableDictionary alloc] init];
     }
-    return _itemDictionary;;
+    return _itemInfoDictionary;;
+}
+
+- (NSMutableArray *)itemTempArray {
+    if (!_itemTempArray) {
+        _itemTempArray = [[NSMutableArray alloc] init];
+    }
+    return _itemTempArray;
+}
+
+- (NSMutableDictionary *)itemObjDictionary {
+    if (!_itemObjDictionary) {
+        _itemObjDictionary = [[NSMutableDictionary alloc] init];
+    }
+    return _itemObjDictionary;
 }
 
 @end
